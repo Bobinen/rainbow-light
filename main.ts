d542bfd193804d9a0b203398d3fc527e58a2ec61
Bobinen <91364071+Bobinen@users.noreply.github.com>
2021-09-25 15:11:17 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let LED_number = 0
+let item = neopixel.create(DigitalPin.P1, 7, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    LED_number = pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1000,
+    0,
+    7
+    )
+    item.clear()
+    item.range(0, LED_number).showRainbow(1, 180)
+    item.show()
 })
